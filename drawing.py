baae2740b121758813c4df796433ed399c1b3edc
Daniel Schreij <dschreij@gmail.com>
2015-05-02 12:52:01 +0200

    First commit

---
+++ drawing.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 12:47:20 2015
+
+@author: Daniel Schreij
+
+[Insert Licence info here]
+
+"""
+
+import sdl2.ext
+from sdl2 import sdlgfx
+from sdl2 import sdlimage
+
+class FrameBuffer(object):
+	
+	def __init__(self):
+		pass
+
+
+class DisplayContext(object):
+	
+	def __init__(self):
+		pass
+
+def create_window((width,height)):
+	sdl2.ext.init()
+	
+	return DisplayContext()
+	

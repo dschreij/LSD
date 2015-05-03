a4eaa0d2dc1608c38a3ebb8c4262cbbc53d01d2a
dschreij <dschreij@gmail.com>
2015-05-03 23:33:16 +0200

    Started with FrameBuffer class and drawing to texture

--- LSD/__init__.py
+++ LSD/__init__.py
@@ -12,21 +12,93 @@ from __future__ import unicode_literals
 
 # SDL2 libraries
 import sdl2.ext
+import LSD.drawing
 
-class DisplayContext(object):
+class SDL2Environment(object):
 	
-	def __init__(self, window, resolution, display_driver):
+	def __init__(self, window, resolution, renderer, factory):
 		self.window = window
 		self.resolution = resolution
-		self.display_driver = display_driver
-		
-	def print_contents(self):
-		print("Resolution: {0}".format(self.resolution))
-		print("Display driver: {0}".format(self.display_driver))
-		
+		self.renderer = renderer
+		self.factory = factory
 
-def create_window((width,height),title="LSD"):
+		# Get the rest of the info by quering SDL2 itself
+		# Only for screen 1 for now
+		self.pysdl2_version = sdl2.__version__
+		self.sdl2_version = "{}.{}.{}".format(sdl2.version.SDL_MAJOR_VERSION, sdl2.version.SDL_MINOR_VERSION, sdl2.version.SDL_PATCHLEVEL)
+		
+		self.cpu_count = sdl2.SDL_GetCPUCount()		
+		self.display_count = sdl2.SDL_GetNumVideoDisplays()
+		self.display_driver = sdl2.SDL_GetCurrentVideoDriver()
+
+		self.displays = []		
+		for dispnum in range(self.display_count):
+			dispinfo = sdl2.SDL_DisplayMode()
+			res = sdl2.SDL_GetCurrentDisplayMode(dispnum, dispinfo)
+			if res == 0:
+				self.displays.append(dispinfo)
+
+		
+		
+	def __str__(self):
+		infostring = """
+SDL2 environment information		
+		
+General:
+	PySDL2 version: {}
+	SDL2 version: {}
+	CPU count: {}
+	
+Displays:
+	Display driver: {}
+	Number of displays detected: {}
+		""".format(
+			self.pysdl2_version,
+			self.sdl2_version,
+			self.cpu_count,
+			self.display_driver,
+			self.display_count
+		)
+		
+		for dispnum, display in enumerate(self.displays):
+			infostring += """
+	Display {}:		
+		Resolution: ({},{})
+		Refresh rate: {} fps
+			""".format(dispnum, display.w, display.h, display.refresh_rate)
+		return infostring
+		
+	def get_available_display_drivers(self):
+		drivers = []
+		for vd in range(sdl2.SDL_GetNumVideoDrivers()):
+			drivers.append(sdl2.SDL_GetVideoDriver(vd))
+		return drivers
+	
+		
+def create_window(resolution,title="SDL2 Display Window"):
+	global current_sdl2_environment 
+	if type(resolution) != tuple and len(resolution) != 2:
+		raise TypeError("Please make sure the resolution variable is a tuple with (width,height)")		
+	(width,height) = resolution
 	sdl2.ext.init()
 	window = sdl2.ext.Window(title, size=(width, height))
 	window.show()
-	return DisplayContext(window,(width,height), sdl2.SDL_GetVideoDriver(0))
+	
+	# Create a render system that renders to the window surface		
+	renderer = sdl2.ext.Renderer(window)
+	renderer.clear(0)
+	# Create sprite factory to create surfaces with later
+	factory = sdl2.ext.SpriteFactory(renderer=renderer)	
+	
+	sdl2env = SDL2Environment(window,resolution,renderer,factory)
+	current_sdl2_environment= sdl2env
+	window.refresh()
+	return sdl2env
+	
+def destroy_window():
+	global current_sdl2_environment 
+	current_sdl2_environment = None
+	sdl2.ext.quit()
+	
+current_sdl2_environment = None
+	
--- LSD/drawing.py
+++ LSD/drawing.py
@@ -18,8 +18,75 @@ import sdl2.ext
 from sdl2 import sdlgfx
 from sdl2 import sdlimage
 
+# misc
+import LSD
+from functools import wraps
+
 class FrameBuffer(object):
 	
-	def __init__(self):
-		pass
+	def __init__(self, environment):
+		if type(environment) != LSD.SDL2Environment:
+			raise TypeError("ERROR: context argument should be of type LSD.SDL2Environment")
+		self.environment = environment	
+		self.surface = environment.factory.create_sprite(size=environment.resolution, access=sdl2.SDL_TEXTUREACCESS_TARGET)
+		self.renderer = environment.renderer
+		self.sdl_renderer = self.renderer.renderer
+		
+		self.clear()		
 	
+	# Decorator
+	def to_texture(drawing_function):
+		
+		@wraps(drawing_function)
+		def wrapped(inst, *args, **kwargs):	
+			tex_set = sdl2.SDL_SetRenderTarget(inst.sdl_renderer, inst.surface.texture)
+			if tex_set != 0:
+				raise Exception("Could not set FrameBuffers texture as rendering target")
+			result = drawing_function(inst, *args, **kwargs)
+			tex_unset = sdl2.SDL_SetRenderTarget(inst.sdl_renderer, None)
+			if tex_unset != 0:
+				raise Exception("Could not release FrameBuffers texture as rendering target")
+			return result
+		return wrapped
+
+
+	@to_texture	
+	def clear(self, color=0):
+		color = sdl2.ext.convert_to_color(color)
+		self.renderer.clear(color)	
+	
+	@to_texture
+	def draw_circle(self, x, y, r, color, fill=True, aa=False):
+		color = sdl2.ext.convert_to_color(color)
+		if fill:		
+			sdlgfx.filledCircleColor(self.sdl_renderer, x, y, r, color)
+		elif aa:
+			sdlgfx.aacircleColor(self.sdl_renderer, x, y, r, color)
+		else:
+			sdlgfx.circleColor(self.sdl_renderer, x, y, r, color)
+	
+	@to_texture	
+	def draw_rect(self, x, y, w, h, color, border_radius=0, fill=True):
+		color = sdl2.ext.convert_to_color(color)
+		
+		if fill:						
+			if border_radius:
+				sdl2.sdlgfx.roundedBoxColor(self.sdl_renderer, x, y, x+w, y+h, border_radius, color)
+			else:
+				sdl2.sdlgfx.boxColor(self.sdl_renderer, x, y, x+w, y+h, color)
+		else:
+			if border_radius:
+				sdl2.sdlgfx.roundedRectangleColor(self.sdl_renderer, x, y, x+w, y+h, border_radius, color)
+			else:
+				sdl2.sdlgfx.rectangleColor(self.sdl_renderer, x, y, x+w, y+h, color)
+
+				
+	def show(self):
+		self.renderer.copy(self.surface)
+		self.renderer.present()
+		self.environment.window.refresh()
+		
+	def __del__(self):
+		del([self.renderer, self.surface, self.sdl_renderer])
+
+		
